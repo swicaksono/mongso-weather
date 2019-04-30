6d06cf345388ce7e75d1a94b75526d9f2b6b4fc0
Sony Wicaksono <sonyayahab@gmail.com>
2019-04-30 15:32:52 +0800

    Fixing failed bind $PORT node.js in heroku

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Example app listening on port 5000!')
 })
 
